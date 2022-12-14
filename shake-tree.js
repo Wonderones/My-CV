028d26ac684b4a14ad6a5e2800c752a073ddc3fa
Wonderones <115393507+Wonderones@users.noreply.github.com>
2022-12-15 02:43:00 +0800

    Update shake-tree.js

--- shake-tree.js
+++ shake-tree.js
@@ -1,3 +1,34 @@
 function myFunction() {
-   location.reload();
+  // location.reload();
+   click();
 }
+
+
+//Hide tree and show present function after 30 Clicks
+let xmasTreeSection = document.querySelector(".image-stack");
+let presentSection = document.querySelector(".present-container");
+
+let clickCount = 0;
+let xmasTreeClick = document.querySelector("#xmas-tree-image");
+
+xmasTreeClick.addEventListener("click", function () {
+
+    if (clickCount  >5) {
+    xmasTreeClick.style.display = 'none';
+    presentSection.style.display = 'block';
+
+      console.log(clickCount);
+ }
+      clickCount  = clickCount +1;
+});
+
+
+
+//$("#xmas-tree-image").click(function(){
+//  clickCount = clickCount + 1;
+//  alert("count")
+//});
+//
+//if (clickCount > 3) {
+//  (".image-stack").style.display = "none;"
+//};
