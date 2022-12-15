145145d0f37481c124796ce9485775bdb14b2bdb
Wonderones <115393507+Wonderones@users.noreply.github.com>
2022-12-16 03:21:00 +0800

    Update shake-tree.js

--- shake-tree.js
+++ shake-tree.js
@@ -3,32 +3,65 @@ function myFunction() {
    click();
 }
 
-
 //Hide tree and show present function after 30 Clicks
+let startContainer = document.querySelector(".initiate-message-container");
+let startButton = document.querySelector("#start-button");
+
+let startMessageContainer = document.querySelector(".start-message-container");
 let xmasTreeSection = document.querySelector(".image-stack");
+
+let presentMessageContainer = document.querySelector(".present-message-container");
 let presentSection = document.querySelector(".present-container");
+let readMessageButton = document.querySelector("#letter-button");
+
+let myMessageContainer = document.querySelector(".my-message-container");
+
 
 let clickCount = 0;
 let xmasTreeClick = document.querySelector("#xmas-tree-image");
 
-xmasTreeClick.addEventListener("click", function () {
+var audioTreeClick = new Audio("music/tree-shake.mp3");
+var audioPresent = new Audio("music/xmassong2.mp3");
+var xmasMusic = new Audio("music/xmassong1.mp3");
+var messageMusic = new Audio("music/lastsong.mp3");
 
-    if (clickCount  >50) {
+
+//After click im ready to start to trigger music and show xmas tree
+startButton.addEventListener("click", function () {
+    xmasMusic.play();
+    startContainer.style.display = 'none';
+    startMessageContainer.style.display = 'block';
+    xmasTreeSection.style.display = 'block';
+ });
+
+//after 50 click trees show present
+xmasTreeClick.addEventListener("click", function () {
+    audioTreeClick.play();
+    if (clickCount > 50) {
+    startMessageContainer.style.display = 'none';
     xmasTreeClick.style.display = 'none';
+    presentMessageContainer.style.display = 'block';
     presentSection.style.display = 'block';
 
       console.log(clickCount);
+    xmasMusic.pause();
+    audioPresent.play();
+
  }
       clickCount  = clickCount +1;
+
 });
 
 
+//after click read letter startButton
 
-//$("#xmas-tree-image").click(function(){
-//  clickCount = clickCount + 1;
-//  alert("count")
-//});
-//
-//if (clickCount > 3) {
-//  (".image-stack").style.display = "none;"
-//};
+
+
+readMessageButton.addEventListener("click", function () {
+  presentMessageContainer.style.display = 'none';
+  presentSection.style.display = 'none';
+  myMessageContainer.style.display = 'block';
+  audioPresent.pause();
+  messageMusic.play();
+
+});
